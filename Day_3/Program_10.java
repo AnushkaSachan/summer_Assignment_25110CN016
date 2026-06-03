e0a153d7ea0bd06b62d48b92388ba0bf04c6c477
AnushkaSachan <anushkasachan9bnpc@gmail.com>
2026-06-03 22:10:03 +0530

    Add files via upload

---
+++ Day_3/Program_10.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Program_10                          // Program to find prime numbers between a given range
+{
+    public static void main (String args [])
+    {
+        int n, i, j, k, check;
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the range between which prime numbers are to be found : ");
+        i=sc.nextInt();
+        j=sc.nextInt();
+
+        System.out.print("Prime numbers between "+i+" and "+j+" are : ");
+
+        for (n=i; n<=j; n++)
+        {
+            check=0;
+            if(n<=1)
+            {
+                check=1;  
+            }
+             
+            for(k=2; k<n/2; k++)
+            {
+                if(n%k==0)
+                {
+                    check=1;
+                    break;
+                }
+            }
+
+            if (check==0)
+                System.out.print(n+" ");
+        }
+    }
+}
